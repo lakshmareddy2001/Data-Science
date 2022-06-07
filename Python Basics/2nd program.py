94e91c1d1d64809587ba87cab895774a747cd6f1
lakshmareddy2001 <79352428+lakshmareddy2001@users.noreply.github.com>
2022-06-07 14:14:53 +0530

    Update 2nd program.py

--- Python Basics/2nd program.py
+++ Python Basics/2nd program.py
@@ -1,3 +1,13 @@
+""" Write a python program to create a data base having 10 employee data each employee information should have 
+            Student Roll No
+            Name of the Student
+            Marks in SSC
+            Marks in Intermediate
+            CGPA util the current Semester
+            Most preffered programing Language
+            Carrier Option
+ """
+
 students = {501:["Lakshma Reddy",9.5,962,8.85,"Python","IT Job"],
             502:["Sujith Kumar Reddy",9.7,962,8.85,"JAVA","IT Job"],
             503:["Maha Lakshmi",9.3,962,8.85,"Python","Masters"],
@@ -9,4 +19,5 @@ students = {501:["Lakshma Reddy",9.5,962,8.85,"Python","IT Job"],
             509:["Karthik",10,962,8.85,"Python","Masters"],
             510:["Amith",8.8,962,8.85,"Java","Masters"],
             }
-print(sudents)
+for i in students:
+            print("The details of the Student " + i + " are " + students[i])
