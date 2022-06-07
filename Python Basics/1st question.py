8b4b90c218f348febbf6836cd81c7b3a192eb4ae
lakshmareddy2001 <79352428+lakshmareddy2001@users.noreply.github.com>
2022-06-07 14:28:49 +0530

    Update 1st question.py

--- Python Basics/1st question.py
+++ Python Basics/1st question.py
@@ -5,17 +5,27 @@
     below 1000000 - 15% of tax
     above 1000000 - 20% of tax
 """
+
 a = float(input("Enter the Anuual Salary of the Employee"))
 b = float(input("Enter the Annaul HRA salary of the Employee"))
 c = float(input("Enter the Deductions of the monthly salary"))
+
 if(c>80000):
     c = 80000
+    
 a = a- b- c
+if(a>300000):
+    a = a - 300000
+    
+    
 if(a<300000):
     print(0)
+    
 elif(a<600000):
     print(a*0.1)
+    
 elif(a<1000000):
     print(a*0.15)
+    
 else:
     print(a*0.2)
